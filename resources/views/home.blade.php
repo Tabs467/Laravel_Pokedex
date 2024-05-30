d2e2a5325cc482354618ae8555398731b3f84d3d
Andrew Taberner <andrewtaberner467@gmail.com>
2024-05-30 16:29:05 +0100

    Simple home page with layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+    <div class="flex flex-col">
+        <div class="mx-auto py-40">
+            <h1 class="font-extrabold font-mono text-7xl">Pokédex</h1>
+        </div>
+</x-layouts.app>
